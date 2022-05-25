f6d9b9f47e048ea6bb2b45e06d13df2482a6b713
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-05-25 10:39:46 +0600

    adding user server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,6 +19,28 @@ async function run() {
         const toolsCollection = client.db("toolsCollection").collection("tools");
         const usersCollection = client.db("toolsCollection").collection("user");
 
+        function verifyJwt(req, res, next) {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'UnAuthorized Access' });
+            }
+            const token = authHeader.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+                if (err) {
+                    return res.status(403).send({ message: 'Forbidden Access' })
+                }
+                req.decoded = decoded;
+                next();
+            });
+
+        }
+
+        app.get('/user', async (req, res) => {
+
+            const users = await usersCollection.find().toArray();
+            res.send(users);
+        });
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -29,15 +52,16 @@ async function run() {
                 },
             }
             const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
-            res.send(result);
+            res.send({ result, token });
         })
         app.get('/tools', async (req, res) => {
             const query = {};
             const tools = await toolsCollection.find(query).toArray();
             res.send(tools);
         })
-        app.get('/tools/:id', async (req, res) => {
+        app.get('/tools/:id', verifyJwt, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const tool = await toolsCollection.findOne(query);
